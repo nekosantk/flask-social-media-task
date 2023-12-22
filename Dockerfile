93d524d404fff53fec0f475eea56e6b2d10a383c
nekosantk <24228731+nekosantk@users.noreply.github.com>
2023-12-22 17:09:38 +0200

    Update dockerfiles + added social activity endpoint

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ WORKDIR app
 RUN apt-get update && apt-get install --no-install-recommends -y curl build-essential
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-COPY . .
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,9 +1,46 @@
+import aiohttp
 from flask import Flask
+import asyncio
 
 app = Flask(__name__)
 
+async def fetch_data(url):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                return url, await response.json()
+    except aiohttp.ClientError as ex:
+        return url, ex
+
+
 @app.route("/")
 def social_network_activity():
-    # TODO: your code here
-    json_response = {}
-    return json_response
+    endpoints = [
+        "https://takehome.io/twitter",
+        "https://takehome.io/facebook",
+        "https://takehome.io/instagram"
+    ]
+
+    tasks = [fetch_data(endpoint) for endpoint in endpoints]
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    responses = loop.run_until_complete(asyncio.gather(*tasks))
+
+    response_data = {}
+
+    for url, response in responses:
+        if isinstance(response, Exception):
+            print(f"Failed to fetch data from {response}: {response}", flush=True)
+        else:
+            print(f"Response from {url}: {response}...", flush=True)
+            short_url = url.split("/")[-1]
+            activity_level = len(response)
+            response_data.update({ short_url: activity_level })
+
+    return response_data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
